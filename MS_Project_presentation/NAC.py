e9793fe65ae32d4a8ea427a595c2c60b563d8560
EliousMondal <mdeliousali@gmail.com>
2020-12-17 12:24:48 +0530

    modified the NAC and nuclear propagation images

--- MS_Project_presentation/NAC.py
+++ MS_Project_presentation/NAC.py
@@ -20,6 +20,11 @@ y11 = np.array([V11(i) for i in x])
 y22 = -y11
 y12 = np.array([V12(i) for i in x])
 
+out_file = open("outfile.txt","w")
+for i in range(len(x)):
+    out_file.write("{:14.8f}    {:14.8f}    {:14.8f}    {:14.8f}\n".format(x[i],y11[i],y22[i],y12[i]))
+out_file.close()
+
 plt.plot(x,y11,label=r"$V_{11}$")
 plt.plot(x,y22,label=r"$V_{22}$")
 plt.plot(x,5*y12,label=r"$V_{12}$")
